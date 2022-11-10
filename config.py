c7d4ebaeadfde719bb2b0850042cea893c29a433
Kevin McCurley <github@digicrime.com>
2022-11-10 11:07:37 -0800

    This works.

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 class Config(object):
     DEBUG = True
     DEVELOPMENT = True
-    DB_PATH='search/xapian.db'
-    prefix = '/awful'
+    DB_PATH='/home/mccurley/git/fundreg/search/xapian.db'
 
 class ProductionConfig(Config):
-    DEBUG = True
-    DEVELOPMENT = True
+    DEBUG = False
+    DEVELOPMENT = False
     # Where you mount the app in mod_wsgi.
     SCRIPT_NAME = '/funding'
-    DB_PATH='/home/mccurley/git/fundreg/search/xapian.db'
+    DB_PATH='search/xapian.db'
 
