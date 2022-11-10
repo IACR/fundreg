19d8561d6cf0d2029e61f9fcb625903c8c4ea661
Kevin McCurley <github@digicrime.com>
2022-11-10 11:08:52 -0800

    Remove my stuff.

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config(object):
     DEBUG = True
     DEVELOPMENT = True
-    DB_PATH='/home/mccurley/git/fundreg/search/xapian.db'
+    DB_PATH='search/xapian.db' # may need to be absolute path.
 
 class ProductionConfig(Config):
     DEBUG = False
     DEVELOPMENT = False
     # Where you mount the app in mod_wsgi.
     SCRIPT_NAME = '/funding'
-    DB_PATH='search/xapian.db'
+    DB_PATH='search/xapian.db' # may need to be absolute path.
 
